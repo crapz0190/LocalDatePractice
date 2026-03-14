99f5e7ab89a0884f5cd956207bd9de68e3ab38cf
Claudio Arispe <crapz0190@gmail.com>
2026-03-14 11:41:39 -0300

    exercise: Subtracting hours and adding minutes

---
+++ src/DateTimeFormatter.java
@@ -0,0 +1,19 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class DateTimeFormatter {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String date = sc.nextLine();
+        String[] data = sc.nextLine().split(" ");
+        int hours = Integer.parseInt(data[0]);
+        int minutes = Integer.parseInt(data[1]);
+        sc.close();
+
+        LocalDateTime dateTime = LocalDateTime.parse(date);
+        dateTime = dateTime.minusHours(hours).plusMinutes(minutes);
+
+        System.out.println(dateTime.format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")));
+    }
+}
